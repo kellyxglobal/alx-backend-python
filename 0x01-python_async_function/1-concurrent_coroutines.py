fccdb5e23434e93c042ee71daa4d900e9d2fa5c2
kellyxglobal <kellyxglobal@gmail.com>
2023-04-07 17:36:13 +0000

    Here is an implementation of the wait_n function that imports the wait_random_time coroutine from the given code and spawns it n times with the specified max_delay argument: The wait_n function uses a list of tasks to spawn n instances of the wait_random_time coroutine with the specified max_delay argument. It then waits for all the tasks to complete using asyncio.gather, which returns a list of the delay times. The delay times are sorted in ascending order using the sorted function, which works correctly even in the presence of concurrency. The sorted list of delay times is then returned by the wait_n function.

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,1 +1,46 @@
+#!/usr/bin/env python3
 
+
+import asyncio
+from typing import List
+from random import uniform
+from asyncio import Task
+
+
+async def wait_random_time(max_delay: int = 10) -> float:
+    """Asynchronous coroutine that waits for a random delay
+    between 0 and max_delay seconds and eventually returns it.
+
+    Args:
+        max_delay: An integer representing the maximum delay time
+            (default is 10 seconds).
+
+    Returns:
+        A float representing the amount of time
+        waited for (between 0 and max_delay seconds).
+    """
+    delay = uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """Asynchronous routine that spawns wait_random_time n times
+    with the specified max_delay argument and returns a list of the
+    delay times in ascending order.
+
+    Args:
+        n: An integer representing the number of times to spawn
+            the wait_random_time coroutine.
+        max_delay: An integer representing the maximum delay time
+            (default is 10 seconds).
+
+    Returns:
+        A list of floats representing the amount of time waited for
+        (between 0 and max_delay seconds), in ascending order.
+    """
+    tasks: List[Task[float]] = []
+    for i in range(n):
+        tasks.append(asyncio.create_task(wait_random_time(max_delay)))
+    results = await asyncio.gather(*tasks)
+    return sorted(results)

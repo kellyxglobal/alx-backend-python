1f3a8af333f0b2b8be502f4fa202163c384e5caf
kellyxglobal <kellyxglobal@gmail.com>
2023-04-07 17:17:26 +0000

    The function is annotated with type hints, and both the module and the function have a documentation string explaining what they do and what their parameters and return values are. You can check the documentation of the module by running python3 -c "print(__import__("my_module").__doc__)" and the documentation of the function by running python3 -c "print(__import__("my_module").wait_random_time.__doc__)", replacing my_module with the name of your Python module.

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -8,11 +8,9 @@ async def wait_random_time(wait_random: int = 10) -> float:
     """Asynchronous coroutine that waits for a random delay
     between 0 and max_delay (inclusive and float value) seconds and
     eventually returns it.
-
     Args:
         wait_random: An integer representing the maximum delay time
         (default is 10 seconds).
-
     Returns:
         A float representing the amount of time waited for
         (between 0 and wait_random seconds).
